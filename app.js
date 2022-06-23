fc96a160e6b40a9dfb8c16342fc4c0bd00601462
neerajponnada08 <neerajponnada08@gmail.com>
2022-06-23 17:59:57 +0530

    Updated the files

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-x={}
-console.log(typeof x)
+// // x={}
+// // console.log(typeof x)
+// function A(a,b)
+// {
+
+//     return "Running "+a+" "+b;
+// }
+// function A(a,b,c,d)
+// {
+
+//     return "Running "+a+" "+b+c+d;
+// }
+// console.log(A("Hi","Hello"))
+// console.log(A("Hi","Hello","Hei","Ki"))
+console.log(["Maths",1])
+
+console.log(data[6](data[5].name)+" likes "+data[7][2])
